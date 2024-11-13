b665071a4c7d26df2a00c5cde385764a81cd67a3
José Leandro Vilela <112025510+Vilelajlr@users.noreply.github.com>
2024-11-13 00:35:20 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-AOS.init();
+document.addEventListener("DOMContentLoaded", function() {
+
+    AOS.init();
+
+    function removeAOSOnMobile() {
+        if (window.innerWidth < 768) { // Define a largura máxima para remover as animações
+            document.querySelectorAll("[data-aos]").forEach(element => {
+                element.removeAttribute("data-aos");
+                element.removeAttribute("data-aos-delay");
+            });
+        }
+    }
+
+    // Executa a função ao carregar a página
+    removeAOSOnMobile();
+
+    // Executa a função sempre que a tela for redimensionada
+    window.addEventListener("resize", removeAOSOnMobile);
+});
